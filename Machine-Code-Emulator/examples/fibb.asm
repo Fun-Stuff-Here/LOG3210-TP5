2a954348dc1a097253b911814ec510e3e1775add
Nicolas Depelteau <71102008+Depdx@users.noreply.github.com>
2022-04-11 10:33:09 -0400

    ND: full 1

--- Machine-Code-Emulator/examples/fibb.asm
+++ Machine-Code-Emulator/examples/fibb.asm
@@ -52,6 +52,19 @@ CLEAR
 //        b = t;
 
 // TODO:: PUT THE BLOCK 1 HERE !
+LD R0, a
+LD R1, b
+ADD R2, R1, R0
+LD R3, d
+MUL R4, R3, R2
+LD R5, c
+MUL R6, R5, R1
+ADD R7, R4, R6
+MUL R8, R3, R1
+MUL R9, R5, R0
+ADD R10, R8, R9
+ST b, R7
+ST a, R10
 // TODO:: END THE BLOCK 1 HERE ABOVE !
 
 CLEAR
@@ -65,15 +78,25 @@ CLEAR
 //      i = i / 2;
 
 // TODO:: PUT THE BLOCK 2 HERE !
+LD R0, c
+MUL R1, #2, R0
+LD R2, d
+ADD R3, R1, R2
+MUL R4, R2, R3
+MUL R5, R0, R0
+MUL R6, R2, R2
+ADD R7, R5, R6
+LD R0, i
+DIV R2, R0, #2
+ST i, R2
+ST d, R4
+ST c, R7
 // TODO:: END THE BLOCK 2 HERE ABOVE!
 
 
 
 
 // TODO:: This instruction is just a placeholder to let the code end, remove the code below!
-LD R0, i
-DEC R0
-ST i, R0
 // TODO:: Remove the placeholder above of this line!
 
 CLEAR
--- log3210-tp5/src/analyzer/visitors/PrintMachineCodeVisitor.java
+++ log3210-tp5/src/analyzer/visitors/PrintMachineCodeVisitor.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Vector;
 
+
+
 public class PrintMachineCodeVisitor implements ParserVisitor {
 
     private PrintWriter m_writer = null;
@@ -20,7 +22,7 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
 
     private HashMap<String,String> OP; // HashMap des Opérations
 
-    private ArrayList<Integer> USE_QUEUE; // potentiellement, stocker l'ordre d'utilisation des Registres (pour savoir lequel est le plus vieux)
+    private ArrayList<String> USE_QUEUE; // potentiellement, stocker l'ordre d'utilisation des Registres (pour savoir lequel est le plus vieux)
     private ArrayList<String> MODIFIED; // potentiellement, garder une liste des variables modifiées (assigned) pour savoir s'il faut faire un ST
 
     public PrintMachineCodeVisitor(PrintWriter writer) {
@@ -51,6 +53,9 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
         node.childrenAccept(this, null);
 
         // TODO: vider REGISTERS (et faire les ST en conséquence)
+        for(String variable :this.MODIFIED)
+            if(this.OUT.get(this.NODE-1).contains(variable))
+            this.printStore(variable);
         m_writer.close();
         return null;
     }
@@ -123,7 +128,12 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
         String left = (String) node.jjtGetChild(1).jjtAccept(this, null);
         String right = (String) node.jjtGetChild(2).jjtAccept(this, null);
 
+        String source1 = this.getReg(right, this.NODE,null );
+        String source2 = this.getReg(left, this.NODE,null );
 
+        String destination = this.getReg(assigned, this.NODE,null );
+
+        this.printOp(node.getOp(),destination,source2,source1);
         // TODO: Chaque variable a son emplacement en mémoire, mais si elle est déjà dans un registre, ne la rechargez pas!
         // TODO: Si une variable n'est pas vive, ne l'enregistrez pas en mémoire.
         // TODO: Si vos registres sont pleins, déterminez quelle variable vous allez retirer et si vous devez la sauvegarder
@@ -152,9 +162,14 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
         String assigned = (String) node.jjtGetChild(0).jjtAccept(this, null);
         String left = (String) node.jjtGetChild(1).jjtAccept(this, null);
 
+        String source = this.getReg(left, this.NODE,null );
+        int index = this.indexForRegister(source);
+        this.removeReg(assigned);
+        this.REGISTERS.get(index).add(assigned);
         // Lors d'une assignation directe, le registre de left est partagé avec 
         // assigned. Si left n'est plus une variable vive dans out, on peut
         // l'enlever de REGISTERS et le remplacer par assigned.
+        this.MODIFIED.add(assigned);
         return null;
     }
 
@@ -175,17 +190,24 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
 
 
     /*
-     * Fonction qui pourrait être utiles dans votre implémentation. 
+     * Fonction qui pourrait être utiles dans votre implémentation.
      * Vous n'êtes pas obligé de les complêter ou de les utiliser.
      */
     public String setReg(String src, int i) {
-        // TODO : Met une variable "src" dans le registre "i". Retourne le nom du registre 
+        // TODO : Met une variable "src" dans le registre "i". Retourne le nom du registre
         return "R" + i;
     }
-    
+
     public String getReg(String src, int node, ArrayList<Vector<String>> maybe_dead) {
         // TODO 1: if exists, get existing register and return
+        int associatedRegister = this.findAssociatedRegister(src);
+        if(associatedRegister != -1) return "R"+associatedRegister;
         // TODO 2: if there is an empty register, put in empty register and return
+        int nextEmptyRegister = this.findNextRegister();
+        if(nextEmptyRegister != -1){
+            this.REGISTERS.get(nextEmptyRegister).add(src);
+            return "R"+ nextEmptyRegister;
+        }
         // TODO 3: if there if dead variables in registers, put in dead register and return
         // TODO 4: other register selection (ex: put in oldest register and return)
 
@@ -194,7 +216,61 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
 
     public void removeReg(String src) {
         // TODO : enlève une string du registers
+        int index = this.findAssociatedRegister(src);
+        if(index == -1) return;
+        this.REGISTERS.get(index).remove(src);
         // Attention de voir s'il faut faire un ST ou non... (ST si c'est une variable vive)
+    }
+
+    private int findAssociatedRegister(String variable){
+        for(int i=0; i <this.REGISTERS.size();i++){
+            if(this.REGISTERS.get(i).contains(variable)) return i;
+        }
+        return -1;
+    }
+
+    private int findNextRegister(){
+        for(int i=0; i <this.REGISTERS.size();i++){
+            if(this.REGISTERS.get(i).size() == 0) return i;
+        }
+        return -1;
+    }
+
+    private void printLoad(String register, String variable){
+        String toPrint = "LD "+ register + ", "+variable;
+        m_writer.println(toPrint);
+    }
+
+    private void printOp(String op, String destination, String source1, String source2){
+        this.updateUse(source2);
+        this.updateUse(source1);
+        this.USE_QUEUE.add(destination);
+
+        String toPrint = this.OP.get(op)+" "+ destination + ", "+source1 + ", "+source2;
+        m_writer.println(toPrint);
+    }
+
+    private void updateUse(String register){
+        if(this.USE_QUEUE.contains(register))return;
+        this.USE_QUEUE.add(register);
+        String variable = this.getVariableFromRegister(register);
+        this.printLoad(register,variable);
+    }
+
+    private String getVariableFromRegister(String register){
+        int index= this.indexForRegister(register);
+        if(this.REGISTERS.get(index).size() <1) return "";
+        return this.REGISTERS.get(index).get(0);
+    }
+
+    private int indexForRegister(String register){
+        return Integer.parseInt(register.substring(1));
+    }
+
+    private void printStore(String variable){
+        int registerIndex = this.findAssociatedRegister(variable);
+        String toPrint = "ST "+ variable + ", R"+registerIndex ;
+        m_writer.println(toPrint);
     }
 
 }
